d05cb7b8fe4fb1bf2c495c99c1726a71b7872968
Dean Tarisai <prjctimg@outlook.com>
2024-03-02 16:35:21 +0200

    Added DOM fixup for icon strokeWidth

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Set the stroke-width for all icons
+
+var icons = document.querySelectorAll("svg");
+
+for (const icon of icons) {
+  icon.style.strokeWidth = "1.5";
+}
+
+// Reading progress tracker
